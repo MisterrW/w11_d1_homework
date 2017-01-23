fa87ef6e7a420266cbb3cceab57fdc7ffbc05d34
William Robertson <w.robertson305@gmail.com>
2017-01-23 15:47:01 +0000

    can now add image url when creating cat entry

--- public/app.js
+++ public/app.js
@@ -4,6 +4,13 @@ var createCat = function(){
   return cat;
 }
 
+var createImg = function(imgUrl){
+  var img = document.createElement('img');
+  img.src = imgUrl;
+  img.width = "500";
+  return img;
+}
+
 var createLi = function(text){
   var li = document.createElement('li');
   li.innerText = text;
@@ -15,31 +22,32 @@ var createName = function(name){
 }
 
 var createFood = function(food){
-  return createLi("Food: " + food);
+  return createLi("Favourite food: " + food);
 }
 
-var appendElements = function(cats, cat, name, food){
+var appendElements = function(cats, cat, name, food, image){
   cat.appendChild(name);
   cat.appendChild(food);
-  // cat.appendChild(image);
+  cat.appendChild(image);
   cats.appendChild(cat);
 }
 
-var addCat = function(name, food){
+var addCat = function(name, food, imgUrl){
   var cats = document.querySelector('#cats');
   var cat = createCat();
   var name = createName(name);
   var food = createFood(food);
-  // var image = createImage(picture);
-  appendElements(cats, cat, name, food);
+  var image = createImg(imgUrl);
+  appendElements(cats, cat, name, food, image);
 }
 
 var app = function(){
-  var cats = [{name: "Dibble", food: "Tuna Chunks"},
-  {name: "Fred", food: "bits of stuff"},
-  {name: "Dibble", food: "Tuna Chunks"}];
+  var cats = [{name: "Dibble", food: "Tuna Chunks", imgUrl: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  {name: "Fred", food: "bits of stuff", imgUrl: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"},
+  {name: "Maurice the Merciless", food: "Birds and other Small Animals", imgUrl: "http://66.media.tumblr.com/7c5784ea89369c780e782bf10c60315a/tumblr_npb0hlYwhV1u63jaco1_1280.jpg"}
+  ];
   for(var cat of cats){
-    addCat(cat.name, cat.food);
+    addCat(cat.name, cat.food, cat.imgUrl);
   }
 }
 
